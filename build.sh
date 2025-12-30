c81d951c104a8331e6d390c995978fc2d1d5f199
Dario Griffo <dariogriffo@gmail.com>
2025-12-30 18:20:30 +0000

    Fix build

--- build.sh
+++ build.sh
@@ -123,7 +123,7 @@ if [ "$ARCH" = "all" ]; then
     echo ""
     
     # All supported architectures
-    ARCHITECTURES=("amd64" "arm64" "armel" "armhf" "ppc64el" "s390x" "riscv64", "i386")
+    ARCHITECTURES=("amd64" "arm64" "armel" "armhf" "ppc64el" "s390x" "riscv64" "i386")
     
     for build_arch in "${ARCHITECTURES[@]}"; do
         echo "==========================================="
